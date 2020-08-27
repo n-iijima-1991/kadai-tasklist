f53c96f5f11dc588bc9e2b4438ed1f67da43778a
nozomu.iijima <n-iijima-1991@gmail.com>
2020-08-27 22:05:04 +0000

    corrected validation for tasks, deleted unnecessary view file, deleted set_task in tasks controller and reflect error message in tasks

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,8 +1,7 @@
 class TasksController < ApplicationController
   before_action :require_user_logged_in
   before_action :correct_user, only: [:edit,:show, :update, :destroy]
-  before_action :set_task, only: [:show, :edit, :update, :destroy]
-  
+
   def index
     @tasks = current_user.tasks.all.order(id: :desc).page(params[:page])
   end
@@ -48,10 +47,6 @@ class TasksController < ApplicationController
   end
   
   private
-  
-  def set_task
-    @task = Task.find(params[:id])
-  end
   
   def task_params
     params.require(:task).permit(:content, :status)
